14a507b538d02cb58436e57457b7234a2057f995
Fazley Azim Sabbir <fazley.azim.sabbir@gmail.com>
2025-07-17 13:09:07 +0600

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa_prediction",
+    version="0.0.0",
+    author="Sabbir",
+    author_email="fazley.azim.sabbir@gmail.com",
+    packages=find_packages()
+)
